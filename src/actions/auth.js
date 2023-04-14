98c57f60ee85f370d056a1e1e72fc81ab0370901
matias225 <mati.kbza22@gmail.com>
2023-04-14 17:47:13 -0300

    Instaladas nuevas librerias, implementados actions, reducers y hooks

---
+++ src/actions/auth.js
@@ -0,0 +1,60 @@
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
+import { auth, googleAuthProvider } from '../firebase/firebaseConfig';
+import { types } from '../types/types';
+import { updateProfile } from 'firebase/auth';
+import { finishLoading, startLoading } from './ui';
+
+export const startLoginEmailPassword = (email, password) => {
+  return (dispatch) => {
+    dispatch( startLoading() );
+    signInWithEmailAndPassword( auth, email, password )
+      .then( ({ user }) => {
+        dispatch( login(user.uid, user.displayName) );
+        dispatch( finishLoading() );
+      }).catch( (e) => {
+        console.log(e);
+        dispatch( finishLoading() );
+      });
+  }
+}
+
+export const startRegisterWithEmailPasswordName = ( email, password, name ) => {
+  return (dispatch) => {
+    createUserWithEmailAndPassword( auth, email, password )
+      .then( async({ user }) => {
+        await updateProfile(user, { displayName: name });
+        dispatch( login( user.uid, user.displayName ));
+      })
+      .catch( e => {
+        console.log(e);
+      });
+  }
+}
+
+export const startGoogleLogin = () => {
+  return (dispatch) => {
+    signInWithPopup( auth, googleAuthProvider )
+      .then( ({ user }) => {
+        dispatch( login( user.uid, user.displayName ));
+    });
+  }
+}
+
+export const login = (uid, displayName) => ({
+  type: types.login,
+  payload: {
+    uid,
+    displayName
+  }
+}) 
+
+export const startLogout = () => {
+  return async ( dispatch ) => {
+    await signOut(auth);
+    dispatch( logout() );
+  }
+}
+
+export const logout = () => ({
+  type: types.logout
+})
