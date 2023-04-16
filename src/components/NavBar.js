b3950b836e29bb38898180739c949752f9b5f34b
matias225 <mati.kbza22@gmail.com>
2023-04-16 00:27:38 -0300

    Creada navbar con algunos estilos

---
+++ src/components/NavBar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { startLogout } from '../actions/auth';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { NavLink } from 'react-router-dom';
+
+export const NavBar = () => {
+  const { name } = useSelector( state => state.auth );
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch( startLogout() );
+  };
+
+  return (
+    <>
+      <div className='navbar__main'>
+          <ul className='navbar__links'>
+            <li><NavLink to='/'>Radar</NavLink></li>
+            <li><NavLink to='/alerts'>Alertas</NavLink></li>
+          </ul>
+
+          <div className='navbar__user'>
+            <ul className='navbar__links'>
+              <li>
+                {name}
+              </li>
+              <li>
+                <button 
+                  onClick={ handleLogout } 
+                >
+                  Logout
+                </button>
+              </li>
+            </ul>
+          
+          </div>
+      </div>
+    </>
+  )
+}
