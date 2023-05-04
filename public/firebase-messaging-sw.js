c51a88148af099a1256c7ee2d453efddf5ad536d
matias225 <mati.kbza22@gmail.com>
2023-05-04 06:01:02 -0300

    Arreglada configuracion y SW

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,11 +1,11 @@
-// importScripts('https://www.gstatic.com/firebasejs/9.19.1/firebase-app-compat.js');
-// importScripts('https://www.gstatic.com/firebasejs/9.19.1/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.19.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.19.1/firebase-messaging-compat.js');
 // // Import and configure the Firebase SDK
 // // These scripts are made available when the app is served or deployed on Firebase Hosting
 // // If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-importScripts('/__/firebase/9.19.1/firebase-app-compat.js');
-importScripts('/__/firebase/9.19.1/firebase-messaging-compat.js');
-importScripts('/__/firebase/init.js');
+// importScripts('/__/firebase/9.19.1/firebase-app-compat.js');
+// importScripts('/__/firebase/9.19.1/firebase-messaging-compat.js');
+// importScripts('/__/firebase/init.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyDWco6HXJ4pnhngz8UOcyrtiUdwwWjrEe8",
--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  // vapidKey: process.env.REACT_APP_FIREBASE_VAPID_KEY
+  vapidKey: process.env.REACT_APP_FIREBASE_VAPID_KEY
 };
 
 // Initialize Firebase
